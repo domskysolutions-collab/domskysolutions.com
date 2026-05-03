2550175a76696015444c6d34dce1badfbe20b228
domskysolutions-collab <domskysolutions@gmail.com>
2026-05-03 19:21:04 +0200

    Revert "GDPR"
    This reverts commit f74cadab08f1e6e43f0e7d3f90b31a87a535ba80.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { CookieBanner } from './components/CookieBanner';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
 import { ToolsPage } from './pages/ToolsPage';
@@ -59,7 +58,6 @@ export default function App() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
-      <CookieBanner />
     </Router>
   );
 }
